5d888f7cb2dafeeac706e38ed1bbcecac65bbc46
zenercurrent <11849012+zenercurrent@users.noreply.github.com>
2026-01-30 09:35:40 +0800

    Added echo and exit command

--- src/main/java/Benbot.java
+++ src/main/java/Benbot.java
@@ -1,4 +1,7 @@
+import java.util.Scanner;
+
 public class Benbot {
+    private static boolean canExit = false;
 
     /** Prints the lines with horizontal borders */
     private static void print(String... lines) {
@@ -7,6 +10,17 @@ public class Benbot {
             System.out.println(line);
         }
         System.out.println("> ");
+    }
+
+    /** Processes the command given to the bot */
+    private static void process(String command) {
+        // bye: exit program
+        if (command.equals("bye")) {
+            print("Goodbye! It was a nice chat! :)");
+            canExit = true;
+        } else {
+            print(command);
+        }
     }
 
     public static void main(String[] args) {
@@ -19,6 +33,11 @@ public class Benbot {
                 """;
         System.out.println(logo);
         print("Hello there! I am Benbot, your personal assistant.", "How can I help?");
-        print("Goodbye! Hope to see you again!");
+
+        while (!canExit) {
+            Scanner scan = new Scanner(System.in);
+            String command = scan.nextLine();
+            process(command);
+        }
     }
 }
