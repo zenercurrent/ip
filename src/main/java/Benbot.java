50b97547285b1f87737d037e3175e67eb95f7a8d
zenercurrent <11849012+zenercurrent@users.noreply.github.com>
2026-01-30 09:11:24 +0800

    Refactor Duke to Benbot

---
+++ src/main/java/Benbot.java
@@ -0,0 +1,18 @@
+public class Benbot {
+    public static void main(String[] args) {
+        String logo = """
+                ▄▄                ▄▄               \s
+                ██                ██           ██  \s
+                ████▄ ▄█▀█▄ ████▄ ████▄ ▄███▄ ▀██▀▀\s
+                ██ ██ ██▄█▀ ██ ██ ██ ██ ██ ██  ██  \s
+                ████▀ ▀█▄▄▄ ██ ██ ████▀ ▀███▀  ██  \s
+                """;
+        System.out.println(logo);
+        System.out.println("------------------------------");
+        System.out.println("Hello there! I am Benbot, your personal assistant.");
+        System.out.println("How can I help?");
+        System.out.println("------------------------------");
+        System.out.println("Goodbye! Hope to see you again!");
+        System.out.println("------------------------------");
+    }
+}
