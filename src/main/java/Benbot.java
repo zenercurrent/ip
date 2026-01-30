60af37489ace11c784a0117b33bec8893dcf0389
zenercurrent <11849012+zenercurrent@users.noreply.github.com>
2026-01-30 10:46:24 +0800

    Add some personality to the bot

--- src/main/java/Benbot.java
+++ src/main/java/Benbot.java
@@ -8,16 +8,6 @@ public class Benbot {
     /** Flag that allows the bot to terminate */
     private static boolean canExit = false;
 
-
-    /** Prints the lines with horizontal borders */
-    private static void print(String... lines) {
-        System.out.println("------------------------------");
-        for (String line : lines) {
-            System.out.println(line);
-        }
-        System.out.println("> ");
-    }
-
     /** Processes the command given to the bot */
     private static void process(String command) {
         String instruction = command.strip().split(" ")[0];
@@ -26,12 +16,13 @@ public class Benbot {
         switch (instruction) {
         // bye: exit program
         case "bye":
-            print("Goodbye! It was a nice chat! :)");
+            System.out.println("Goodbye! It was a nice chat! :)");
             canExit = true;
             break;
 
         // list: display all tasks
         case "list":
+            System.out.println("These are your current tasks:");
             for (int i = 0; i < tasks.size(); i++) {
                 System.out.println(i + 1 + ". " + tasks.get(i));
             }
@@ -40,14 +31,21 @@ public class Benbot {
         // (un)mark: mark a task as (not) done
         case "mark":
         case "unmark":
-            Task task = tasks.get(Integer.parseInt(parameters));
+            Task task = tasks.get(Integer.parseInt(parameters) - 1);
+
+            if (instruction.equals("mark")) {
+                System.out.println("Good work. This task is done:");
+            } else {
+                System.out.println("I will mark this as undone for now:");
+            }
+            System.out.println("\t" + task);
             task.setDone(instruction.equals("mark"));
             break;
 
         // add as new task
         default:
+            System.out.println("New task: " + command);
             tasks.add(new Task(command));
-            print("added: " + command);
             break;
         }
     }
@@ -61,12 +59,20 @@ public class Benbot {
                 ████▀ ▀█▄▄▄ ██ ██ ████▀ ▀███▀  ██  \s
                 """;
         System.out.println(logo);
-        print("Hello there! I am Benbot, your personal assistant.", "How can I help?");
+        System.out.println("------------------------------");
+        System.out.println("Hello there! I am Benbot, your personal assistant.");
+        System.out.println("How can I help?");
+        System.out.println("------------------------------");
 
         while (!canExit) {
+            System.out.println("> ");
             Scanner scan = new Scanner(System.in);
             String command = scan.nextLine();
+            System.out.println("------------------------------");
             process(command);
+            System.out.println("------------------------------");
         }
+        System.out.println(logo);
+        System.out.println("Isaac Goh");
     }
 }
