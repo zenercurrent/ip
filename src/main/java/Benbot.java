de3241fe9386b1957273e5fc9223b810e9192350
zenercurrent <11849012+zenercurrent@users.noreply.github.com>
2026-01-30 10:00:27 +0800

    Added tasks function

--- src/main/java/Benbot.java
+++ src/main/java/Benbot.java
@@ -1,7 +1,13 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Benbot {
+    /** A collection of saved tasks */
+    private static final ArrayList<String> tasks = new ArrayList<>();
+
+    /** Flag that allows the bot to terminate */
     private static boolean canExit = false;
+
 
     /** Prints the lines with horizontal borders */
     private static void print(String... lines) {
@@ -14,12 +20,19 @@ public class Benbot {
 
     /** Processes the command given to the bot */
     private static void process(String command) {
-        // bye: exit program
         if (command.equals("bye")) {
+            // bye: exit program
             print("Goodbye! It was a nice chat! :)");
             canExit = true;
+        } else if (command.equals("list")) {
+            // list: display all tasks
+            for (int i = 0; i < tasks.size(); i++) {
+                System.out.println(i + 1 + ". " + tasks.get(i));
+            }
         } else {
-            print(command);
+            // add as new task
+            tasks.add(command);
+            print("added: " + command);
         }
     }
 
