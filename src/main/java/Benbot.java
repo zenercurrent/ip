773b840a29d1e4b0430508292634e7a62a52685e
zenercurrent <11849012+zenercurrent@users.noreply.github.com>
2026-01-30 11:29:46 +0800

    Add different tasks support

--- src/main/java/Benbot.java
+++ src/main/java/Benbot.java
@@ -15,24 +15,22 @@ public class Benbot {
 
         switch (instruction) {
         // bye: exit program
-        case "bye":
+        case "bye" -> {
             System.out.println("Goodbye! It was a nice chat! :)");
             canExit = true;
-            break;
+        }
 
         // list: display all tasks
-        case "list":
+        case "list" -> {
             System.out.println("These are your current tasks:");
             for (int i = 0; i < tasks.size(); i++) {
                 System.out.println(i + 1 + ". " + tasks.get(i));
             }
-            break;
+        }
 
         // (un)mark: mark a task as (not) done
-        case "mark":
-        case "unmark":
+        case "mark", "unmark" -> {
             Task task = tasks.get(Integer.parseInt(parameters) - 1);
-
             if (instruction.equals("mark")) {
                 System.out.println("Good work. This task is done:");
             } else {
@@ -40,13 +38,44 @@ public class Benbot {
             }
             System.out.println("\t" + task);
             task.setDone(instruction.equals("mark"));
-            break;
+        }
 
-        // add as new task
-        default:
-            System.out.println("New task: " + command);
-            tasks.add(new Task(command));
-            break;
+        // todo: create a new todo task
+        case "todo" -> {
+            Todo todo = new Todo(parameters);
+            tasks.add(todo);
+            System.out.println("I have created a new task for you:");
+            System.out.println("\t" + todo);
+            System.out.println("There are now " + tasks.size() + " task(s) to be done.");
+        }
+
+        // deadline: create a new deadline task
+        case "deadline" -> {
+            String name = parameters.strip().split(" /by ")[0];
+            String by = parameters.strip().split(" /by ")[1];
+            Deadline deadline = new Deadline(name, by);
+            tasks.add(deadline);
+            System.out.println("I have created a new task for you:");
+            System.out.println("\t" + deadline);
+            System.out.println("There are now " + tasks.size() + " task(s) to be done.");
+        }
+
+        // event: create a new deadline task
+        case "event" -> {
+            String name = parameters.strip().split(" /from | /to ")[0];
+            String from = parameters.strip().split(" /from | /to ")[1];
+            String to = parameters.strip().split(" /from | /to ")[2];
+            Event event = new Event(name, from, to);
+            tasks.add(event);
+            System.out.println("I have created a new task for you:");
+            System.out.println("\t" + event);
+            System.out.println("There are now " + tasks.size() + " task(s) to be done.");
+        }
+
+        // do nothing (for now)
+        default -> {
+            System.out.println("Unknown command...");
+        }
         }
     }
 
