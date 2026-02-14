35907acf56e42fc502c889a25ba48c8bd55b87be
zenercurrent <11849012+zenercurrent@users.noreply.github.com>
2026-02-14 17:15:02 +0800

    Refactor commands to use enums

--- src/main/java/Command.java
+++ src/main/java/Command.java
@@ -1,2 +1,160 @@
-package PACKAGE_NAME;public enum Command {
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * A possible command that can be executed by Zenerbot.
+ * Each command can expect parameters, but the base command is not case-sensitive.
+ */
+public enum Command {
+
+    /** bye: exits program */
+    BYE {
+        @Override
+        void execute(Zenerbot bot, String[] params) {
+            System.out.println("Goodbye! It was a nice chat! :)");
+            bot.terminate();
+        }
+    },
+
+    /** list: display all tasks */
+    LIST {
+        @Override
+        void execute(Zenerbot bot, String[] params) {
+            ArrayList<Task> tasks = bot.getTasks();
+            if (tasks.size() == 0) {
+                System.out.println("No tasks today. Take a break!");
+                return;
+            }
+
+            System.out.println("These are your current tasks:");
+            for (int i = 0; i < tasks.size(); i++) {
+                System.out.println(i + 1 + ". " + tasks.get(i));
+            }
+        }
+    },
+
+    /** mark: mark a task as done */
+    MARK {
+        @Override
+        void execute(Zenerbot bot, String[] params) {
+            ArrayList<Task> tasks = bot.getTasks();
+            int i = Integer.parseInt(params[0]);
+            Task task = tasks.get(i - 1);
+
+            System.out.println("Good work. This task is done:");
+            System.out.println("\t" + task);
+            task.setDone(true);
+        }
+    },
+
+    /** unmark: mark a task as not done */
+    UNMARK {
+        @Override
+        void execute(Zenerbot bot, String[] params) {
+            ArrayList<Task> tasks = bot.getTasks();
+            int i = Integer.parseInt(params[0]);
+            Task task = tasks.get(i - 1);
+
+            System.out.println("I will mark this as undone for now:");
+            System.out.println("\t" + task);
+            task.setDone(false);
+        }
+    },
+
+    /** todo: create a new todo task */
+    TODO {
+        @Override
+        void execute(Zenerbot bot, String[] params) {
+            if (params.length == 0) {
+                throw new InvalidTaskException();
+            }
+
+            ArrayList<Task> tasks = bot.getTasks();
+            Todo todo = new Todo(String.join(" ", params));
+            tasks.add(todo);
+            System.out.println("I have created a new task for you:");
+            System.out.println("\t" + todo);
+            System.out.println("There are now " + tasks.size() + " task(s) to be done.");
+        }
+    },
+
+    /** deadline: create a new deadline task */
+    DEADLINE {
+        @Override
+        void execute(Zenerbot bot, String[] params) {
+            int b = Arrays.asList(params).indexOf("/by");
+            if (b == -1 || b == params.length - 1) {
+                throw new InvalidTaskException();
+            }
+
+            ArrayList<Task> tasks = bot.getTasks();
+            String name = String.join(" ", Arrays.copyOfRange(params, 0, b));
+            String by = String.join(" ", Arrays.copyOfRange(params, b + 1, params.length));
+            Deadline deadline = new Deadline(name, by);
+            tasks.add(deadline);
+            System.out.println("I have created a new task for you:");
+            System.out.println("\t" + deadline);
+            System.out.println("There are now " + tasks.size() + " task(s) to be done.");
+        }
+    },
+
+    /** event: create a new event task */
+    EVENT {
+        @Override
+        void execute(Zenerbot bot, String[] params) {
+            int f = Arrays.asList(params).indexOf("/from");
+            int t = Arrays.asList(params).indexOf("/to");
+            if (f == -1 || f == params.length - 1 || t == -1 || t == params.length - 1 || t - f <= 1) {
+                throw new InvalidTaskException();
+            }
+
+            ArrayList<Task> tasks = bot.getTasks();
+            String name = String.join(" ", Arrays.copyOfRange(params, 0, f));
+            String from = String.join(" ", Arrays.copyOfRange(params, f + 1, t));
+            String to = String.join(" ", Arrays.copyOfRange(params, t + 1, params.length));
+            Event event = new Event(name, from, to);
+            tasks.add(event);
+            System.out.println("I have created a new task for you:");
+            System.out.println("\t" + event);
+            System.out.println("There are now " + tasks.size() + " task(s) to be done.");
+        }
+    },
+
+    /** delete: remove a task from the list */
+    DELETE {
+        @Override
+        void execute(Zenerbot bot, String[] params) {
+            ArrayList<Task> tasks = bot.getTasks();
+            int i = Integer.parseInt(params[0]);   // todo: handle possible exception here
+            Task task = tasks.get(i - 1);
+            tasks.remove(i - 1);
+            System.out.println("Noted with thanks. I have removed the task:");
+            System.out.println("\t" + task);
+            System.out.println("Now you are left with " + tasks.size() + " tasks.");
+        }
+    };
+
+
+    /**
+     * Gets a command based on a string (non-case-sensitive).
+     *
+     * @param cmd the string
+     * @return the command enum
+     */
+    public static Command fromString(String cmd) {
+        try {
+            return Command.valueOf(cmd.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            throw new UnknownCommandException();
+        }
+    }
+
+
+    /**
+     * Executes the defined behaviour of the command.
+     * Can take in parameters.
+     *
+     * @param params the command parameters
+     */
+    abstract void execute(Zenerbot bot, String[] params);
 }
--- src/main/java/Zenerbot.java
+++ src/main/java/Zenerbot.java
@@ -6,7 +6,7 @@ public class Zenerbot {
     private static Zenerbot BOT;
 
     /** A collection of saved tasks */
-    private static final ArrayList<Task> tasks = new ArrayList<>();
+    private final ArrayList<Task> tasks = new ArrayList<>();
 
     /** The welcome logo of Zenerbot! */
     public static final String LOGO =
@@ -36,90 +36,8 @@ public class Zenerbot {
         return BOT;
     }
 
-    /** Processes the command given to the bot */
-    public void process(String command) {
-        String instruction = command.strip().split(" ")[0];
-        String parameters = command.length() > instruction.length()
-                ? command.replaceFirst(instruction + " ", "")
-                : "";
-
-        switch (instruction) {
-        // list: display all tasks
-        case "list" -> {
-            System.out.println("These are your current tasks:");
-            for (int i = 0; i < tasks.size(); i++) {
-                System.out.println(i + 1 + ". " + tasks.get(i));
-            }
-        }
-
-        // (un)mark: mark a task as (not) done
-        case "mark", "unmark" -> {
-            Task task = tasks.get(Integer.parseInt(parameters) - 1);
-            if (instruction.equals("mark")) {
-                System.out.println("Good work. This task is done:");
-            } else {
-                System.out.println("I will mark this as undone for now:");
-            }
-            System.out.println("\t" + task);
-            task.setDone(instruction.equals("mark"));
-        }
-
-        // todo: create a new todo task
-        case "todo" -> {
-            if (command.strip().equals("todo")) {
-                throw new InvalidTaskException();
-            }
-
-            Todo todo = new Todo(parameters);
-            tasks.add(todo);
-            System.out.println("I have created a new task for you:");
-            System.out.println("\t" + todo);
-            System.out.println("There are now " + tasks.size() + " task(s) to be done.");
-        }
-
-        // deadline: create a new deadline task
-        case "deadline" -> {
-            if (!parameters.contains("/by")) {
-                throw new InvalidTaskException();
-            }
-
-            String name = parameters.strip().split(" /by ")[0];
-            String by = parameters.strip().split(" /by ")[1];
-            Deadline deadline = new Deadline(name, by);
-            tasks.add(deadline);
-            System.out.println("I have created a new task for you:");
-            System.out.println("\t" + deadline);
-            System.out.println("There are now " + tasks.size() + " task(s) to be done.");
-        }
-
-        // event: create a new deadline task
-        case "event" -> {
-            if (!parameters.contains("/from") || !parameters.contains("/to")) {
-                throw new InvalidTaskException();
-            }
-
-            String name = parameters.strip().split(" /from | /to ")[0];
-            String from = parameters.strip().split(" /from | /to ")[1];
-            String to = parameters.strip().split(" /from | /to ")[2];
-            Event event = new Event(name, from, to);
-            tasks.add(event);
-            System.out.println("I have created a new task for you:");
-            System.out.println("\t" + event);
-            System.out.println("There are now " + tasks.size() + " task(s) to be done.");
-        }
-
-        case "delete" -> {
-            int i = Integer.parseInt(parameters);   // todo: handle possible exception here
-            Task task = tasks.get(i - 1);
-            tasks.remove(i - 1);
-            System.out.println("Noted with thanks. I have removed the task:");
-            System.out.println("\t" + task);
-            System.out.println("Now you are left with " + tasks.size() + " tasks.");
-        }
-
-        // unknown command
-        default -> throw new UnknownCommandException();
-        }
+    public ArrayList<Task> getTasks() {
+        return this.tasks;
     }
 
     /** Starts and runs the bot loop. */
@@ -133,12 +51,18 @@ public class Zenerbot {
         Scanner scan = new Scanner(System.in);
         // bot loop (continues until termination)
         while (scan.hasNextLine()) {
-            System.out.println("> ");
-            String command = scan.nextLine();
+            String cmd = scan.nextLine();
             System.out.println("------------------------------");
 
             try {
-                process(command);
+//                process(command);
+                String instruction = cmd.strip().split(" ")[0];
+                String parameters = cmd.length() > instruction.length()
+                        ? cmd.replaceFirst(instruction + " ", "")
+                        : "";
+                Command command = Command.fromString(instruction);
+                command.execute(Zenerbot.getInstance(), parameters.split(" "));
+
             } catch (UnknownCommandException | InvalidTaskException e) {
                 System.out.println(e);
             }
