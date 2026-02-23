ffebda1436577b9a97568798e1a165464260ab18
zenercurrent <11849012+zenercurrent@users.noreply.github.com>
2026-02-23 12:10:50 +0800

    Refactor to Storage class

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,27 +1,15 @@
 import java.util.ArrayList;
-import java.util.List;
 
 /**
- * Containing tasks currently stored by the bot. Can be initialised with an array of tasks.
+ * Containing tasks currently stored by the bot.
  */
 public class TaskList extends ArrayList<Task> {
-    private final ArrayList<Task> tasks;
-
-    /**
-     * Instantiates a new task list with an initial array of tasks.
-     *
-     * @param tasks the initial tasks
-     */
-    public TaskList(Task[] tasks) {
-        this();
-        this.tasks.addAll(List.of(tasks));
-    }
 
     /**
      * Instantiates a new empty task list.
      */
     public TaskList() {
-        this.tasks = new ArrayList<>();
+
     }
 
     @Override
